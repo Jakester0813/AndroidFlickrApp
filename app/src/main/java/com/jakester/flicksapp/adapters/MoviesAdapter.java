cec8d3a9cf7c9821ac6322601161c5ed5519afd8
Jake Rushing <jakester0812@gmail.com>
2017-09-16 11:40:00 -0700

    Implemented the View Holder Pattern for the ListView ArrayAdapter

--- app/src/main/java/com/jakester/flicksapp/adapters/MoviesAdapter.java
+++ app/src/main/java/com/jakester/flicksapp/adapters/MoviesAdapter.java
@@ -26,6 +26,12 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
 
     ArrayList<Movie> mMovies;
 
+    private static class MovieViewHolder {
+        TextView mTitle;
+        TextView mDescription;
+        ImageView mPosterImage;
+    }
+
     public MoviesAdapter(@NonNull Context context, ArrayList<Movie> pMovies) {
         super(context, 0, pMovies);
         mMovies = pMovies;
@@ -34,17 +40,24 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         // Lookup view for data population
+        Movie movie = getItem(position);
+        MovieViewHolder viewHolder;
         if (convertView == null) {
+            viewHolder = new MovieViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_row, parent, false);
+            viewHolder.mTitle = (TextView) convertView.findViewById(R.id.tv_movie_title);
+            viewHolder.mDescription = (TextView) convertView.findViewById(R.id.tv_movie_desc);
+            viewHolder.mPosterImage = (ImageView) convertView.findViewById(R.id.iv_movie_poster);
+            //Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (MovieViewHolder) convertView.getTag();
         }
 
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.iv_movie_poster);
-        Picasso.with(parent.getContext()).load(mMovies.get(position).getPosterPath()).fit()
-                .centerCrop().into(imageView);
-        TextView movieTitle = (TextView) convertView.findViewById(R.id.tv_movie_title);
-        movieTitle.setText(mMovies.get(position).getTitle());
-        TextView movieDescription = (TextView) convertView.findViewById(R.id.tv_movie_desc);
-        movieDescription.setText(mMovies.get(position).getOverview());
+        Picasso.with(parent.getContext()).load(movie.getPosterPath()).fit().centerCrop().into(viewHolder.mPosterImage);
+        viewHolder.mTitle.setText(mMovies.get(position).getTitle());
+        viewHolder.mDescription.setText(mMovies.get(position).getOverview());
         return convertView;
 
     }
